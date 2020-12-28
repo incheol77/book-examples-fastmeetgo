1f98dbf1688d903d853936de67c95ef503f5983a
incheol77 <incheol77@gmail.com>
2020-12-28 17:11:15 +0900

    add Println for debugging

--- ch35usingsync/conditionsignal.go
+++ ch35usingsync/conditionsignal.go
@@ -17,11 +17,13 @@ func main() {
 	for i := 0; i < 3; i++ {
 		go func(n int) {
 			mutex.Lock()
+			fmt.Println("producer : Lock")
 			c <- true
 			fmt.Println("wait begin : ", n)
 			cond.Wait()
 			fmt.Println("wait end : ", n)
 			mutex.Unlock()
+			fmt.Println("producer : UnLock")
 		}(i)
 	}
 
@@ -31,9 +33,11 @@ func main() {
 
 	for i := 0; i < 3; i++ {
 		mutex.Lock()
+		fmt.Println("consumer : Lock")
 		fmt.Println("signal : ", i)
 		cond.Signal()
 		mutex.Unlock()
+		fmt.Println("consumer : UnLock")
 	}
 
 	fmt.Scanln()
